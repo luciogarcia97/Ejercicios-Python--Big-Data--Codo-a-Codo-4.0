bb2eae9e86999de6563119fc446c9d44d9611725
Lucio Agustin Garcia <116604189+luciogarcia97@users.noreply.github.com>
2024-03-18 02:32:33 -0300

    Continuamos con el 5

---
+++ Guia de Ejercicios Pseint en python/#05/Desafío Especial #05.py
@@ -0,0 +1,18 @@
+# ● (1) Un docente ingresa por teclado las 3 notas de un alumno (notas enteras), y la aprobación de los
+# trabajos prácticos de la materia (S si están aprobados, N si no están aprobados).
+# ● (2) El programa debe calcular el promedio de las notas y establecer el valor de una variable estado,
+# de forma que contenga alguno de los 4 valores siguientes, según sea el promedio y los TPs del
+# alumno:
+#     "R" - Si el promedio es menor que 4
+#     "D" - Si no tiene aprobados los TPs y el promedio es mayor o igual a 4.
+#     "A" - Si tiene aprobados los TPs y el promedio es menor que 7.
+#     "P" - Si tiene aprobados los TPs y el promedio es mayor o igual que 7.
+# Como se puede observar, cada "estado" excluye a los anteriores.
+# ● (3) Dejá una línea en blanco de separación de los ingresos.
+# ● (4) Según el valor de la variable estado, mostrá por pantalla alguno de los 4 mensajes siguientes:
+
+#     Reprobado. Debe recursar la materia.
+#     Debe presentar los TPs.
+#     Aprobado. Debe rendir el final.
+#     Promocionado.
+# ● (5) Dejá una línea en blanco al final.
--- Guia de Ejercicios Pseint en python/#05/Ejercicio #05-05.py
+++ Guia de Ejercicios Pseint en python/#05/Ejercicio #05-05.py
@@ -3,6 +3,22 @@
 # ● (2) Según la fecha ingresada, mostrá por pantalla un mensaje que indique si corresponde al
 # Verano, Otoño, Invierno o Primavera.
 
-dia = int(input("Ingresa el dia: "))
-mes = int(input("Ingresa el mes: "))
-
+def determinar_estacion(mes):
+    if mes in (1, 2, 12):
+        return "Verano"
+    elif mes in (3, 4, 5):
+        return "Otoño"
+    elif mes in (6, 7, 8):
+        return "Invierno"
+    elif mes in (9, 10, 11):
+        return "Primavera"
+    else:
+        return "Mes inválido"
+    
+# Solicita al usuario ingresar un número de mes (1-12)
+try:
+    numero_mes = int(input("Ingresa el número de mes (1-12): "))
+    estacion = determinar_estacion(numero_mes)
+    print(f"La estación correspondiente al mes {numero_mes} es: {estacion}")
+except ValueError:
+    print("Ingresa un número válido.")
